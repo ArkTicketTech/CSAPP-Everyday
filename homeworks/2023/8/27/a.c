b34d35e81ccbe5b04cd28c77766cf3af1d887ec9
wongdark2017 <a123154@yeah.net>
2023-08-28 09:58:32 +0800

    2.2

---
+++ homeworks/2023/8/27/a.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+    size_t i;
+    for (i = 0; i < len; i++)
+	printf(" %.2x", start[i]);    //line:data:show_bytes_printf
+    printf("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int)); //line:data:show_bytes_amp1
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(float)); //line:data:show_bytes_amp2
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer) &x, sizeof(void *)); //line:data:show_bytes_amp3
+}
+
+void test_show_bytes(int val) {
+    int ival = val;
+    float fval = (float) ival;
+    int *pval = &ival;
+    show_int(ival);
+    show_float(fval);
+    show_pointer(pval);
+}
+
+int main(int argc, char const *argv[])
+{
+    test_show_bytes(1);
+    return 0;
+}
